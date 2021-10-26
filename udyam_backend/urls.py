42d5fb4507612cf35746d89882709cd6537cdfcb
numberbee7070 <yash.garg.cd.ece19@itbhu.ac.in>
2021-10-26 17:16:28 +0530

    flake config and static urls

--- udyam_backend/urls.py
+++ udyam_backend/urls.py
@@ -1,31 +1,18 @@
-"""udyam_backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
 
-admin.site.site_header = 'Udyam Site Backend Administration'
+admin.site.site_header = "Udyam Site Backend Administration"
 
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Udyam Site API",
-        default_version='v1',
+        default_version="v1",
         description="This is the Udyam Site API.",
     ),
     public=True,
@@ -33,9 +20,25 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('auth/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('auth/', include('custom_auth.urls')),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+    path("admin/", admin.site.urls),
+    path("auth/", include("custom_auth.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:
+    # swagger API pages not visible on production
+    urlpatterns += [
+        path(
+            "",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        path(
+            "auth/",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        path(
+            "redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+        ),
+    ]
