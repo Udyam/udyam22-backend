dc40d9c58d18b87415ef8a8edb6fa7e5338e11bb
ayushigupta931 <ayushigupta931@gmail.com>
2021-11-29 16:16:20 +0530

    Queryset added

--- udyam_API/views.py
+++ udyam_API/views.py
@@ -8,30 +8,15 @@ from .serializers import WorkshopSerializer, NoticeBoardSerializer
 class WorkshopView(generics.ListAPIView):
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = WorkshopSerializer
-
-    def list(self, request):
-        workshops = Workshop.objects.all().order_by('-date')
-        serializer = WorkshopSerializer(workshops, many=True)
-
-        return Response(serializer.data)
+    queryset = Workshop.objects.all().order_by('-date')
 
 
 class GetAllNoticeView(generics.ListAPIView):
     serializer_class = NoticeBoardSerializer
-
-    def list(self, request):
-        all_notice = NoticeBoard.objects.all().order_by('-date')
-        serializer = NoticeBoardSerializer(all_notice, many=True)
-
-        return Response(serializer.data)
+    queryset = NoticeBoard.objects.all().order_by('-date')
 
 
 class GetNoticeByIdView(generics.RetrieveAPIView):
     serializer_class = NoticeBoardSerializer
-
-    def retrieve(self, request, id):
-        all_notice = NoticeBoard.objects.all()
-        notice = get_object_or_404(all_notice, pk=id)
-        serializer = NoticeBoardSerializer(notice)
-
-        return Response(serializer.data)
+    queryset = NoticeBoard.objects.all()
+    lookup_field = 'id'
