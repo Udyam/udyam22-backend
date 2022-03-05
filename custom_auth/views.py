16b910fec73ef67a48050b93ed91004020a1f31a
ayushigupta931 <ayushigupta931@gmail.com>
2022-03-05 12:56:02 +0530

    Activate account message

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -55,7 +55,7 @@ class LoginView(generics.GenericAPIView):
             )
         elif user.is_active is False:
             return Response(
-                {"error": "Please verify your email first and then login."},
+                {"error": "You have not activated your account after registration. Please check your mail."},
                 status=status.HTTP_401_UNAUTHORIZED,
             )
 
